cf730d4e1e254a4e92ae31f7c74c3118ef12ce0d
marcelohernanfl <marcelo37393@gmail.com>
2025-09-13 19:44:26 -0300

    Update Trabajo Práctico Repetitivas.py
    Final

--- Unidad 3/Trabajo Práctico Repetitivas.py
+++ Unidad 3/Trabajo Práctico Repetitivas.py
@@ -40,9 +40,41 @@ for numero in range (100, -1, -1):
 num_positivo = int(input("Ingrese un número entero positivo: "))
 while num_positivo <= 0:
     num_positivo = int(input("Por favor, ingrese un número entero positivo: "))
-
 suma = 0
 for i in range(num_positivo + 1):
     suma += i
-
 print("La suma de los números comprendidos entre 0 y", num_positivo, "es", suma)
+#Ejercicio 8
+num_entero= int(input("Ingrese un número entero"))
+iterador=0
+contador_positivos=0
+contador_negativos=0
+contador_pares=0
+contador_impares=0
+for iterador in range (99):
+     iterador+=1
+     num_entero=int(input ("Ingrese un número entero"))
+     if num_entero<0:
+      contador_negativos=contador_negativos+1
+     else:contador_positivos=contador_positivos+1
+     if num_entero % 2 == 0:
+      contador_pares=contador_pares+1
+     else:
+      contador_impares=contador_impares+1
+print(f"El total de números positivos es",contador_positivos, "el total de números negativos es",contador_negativos, "el total de números pares es",contador_pares, "el total de números impares es",contador_impares)
+#Ejercicio 9
+#Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la media
+import statistics
+num_int=int(input ("Ingrese un número entero"))
+lista_numeros=[num_int]
+for i in range (99):
+    i+=1
+    num_int=int(input ("Ingrese un número entero"))
+    lista_numeros.append(num_int)
+print("La media de los números ingresados es", statistics.mean(lista_numeros))
+#Ejercicio 10
+#Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
+#usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
+num_dig=int(input("Ingrese un número entero"))
+for digito in str(num_dig)[::-1]:
+    print(digito, end="")
