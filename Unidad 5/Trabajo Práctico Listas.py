767ca75b9eb561d612ada4e7c2a704588847c54b
marchernanfleitas <marcelo.fleitas@tupad.utn.edu.ar>
2025-09-20 22:30:01 -0300

    Update Trabajo Práctico Listas.py
    Hasta ejercicio 4

--- Unidad 5/Trabajo Práctico Listas.py
+++ Unidad 5/Trabajo Práctico Listas.py
@@ -1,3 +1,4 @@
+#Ejercicio 1
 import statistics   
 mean= statistics.mean
 notas= [5 , 6 , 7 , 8, 5, 9 , 8, 5, 2 , 6 ] 
@@ -11,3 +12,28 @@ for n in notas:
 print (("La nota promedio es"), (mean) )
 print (("La nota mínima de los alumnos es "), (val_min) )
 print (("La nota máxima de los alumnos es "), (val_max) )
+
+#Ejercicio 2
+lista_producto = []
+for i in range (5):
+ producto = (  input ("Ingrese un producto que desea comprar " ) )
+ lista_producto.append(producto)
+print (sorted(lista_producto))
+producto_eliminado= ( input ("Ingrese el producto que desea eliminar "))
+lista_producto.remove (producto_eliminado)
+print (("La lista final de compra es :"), (lista_producto)) 
+
+#Ejercicio 3
+import random
+list_num= []
+pares= []
+impares= [] 
+list_num= [(random.randint(1,100)) for i in range (15)]
+for n in list_num:
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        (impares.append)(impares)
+print (list_num)
+print ( ( "La cantidad de nros pares es :"), (len(pares) ) )
+print ( ( "La cantidad de nros impares es :"), (len(impares)) )
